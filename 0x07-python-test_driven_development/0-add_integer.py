7ea118d216059602ab27755434eecc9ead476da9
cm-abeka <cmabeka2016@gmail.com>
2023-11-11 15:20:34 +0300

    0-int addition comments

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,14 +1,14 @@
 #!/usr/bin/python3
-"""Defines an integer addition function.
-    >>> print(add_integer(100.3, -2))
-    98
-    >>> print(add_integer(4, "School"))
-    b must be an integer """
+"""
+Defines an integer addition function.
+Prototype: def add_integer(a, b=98)
+Returns the sum of the integers a + b
+"""
 
 def add_integer(a, b=98):
-    ''' Returns integer addition a + b
-        Raises type error if a or b is not int or float
-        Floats are casted into integers '''
+    ''' Raises type error if a or b is not int or float
+        Floats are casted into integers 
+    '''
 
     if type(a) not in [float, int]:
         raise TypeError('a must be an integer')
