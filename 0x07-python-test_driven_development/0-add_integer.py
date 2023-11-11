c96581d4d7a9162a381ec17e536c8ae9ebe1b605
cm-abeka <cmabeka2016@gmail.com>
2023-11-11 15:15:38 +0300

    integer addition

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,19 +1,18 @@
-''' Addition Function '''
-
+#!/usr/bin/python3
+"""Defines an integer addition function.
+    >>> print(add_integer(100.3, -2))
+    98
+    >>> print(add_integer(4, "School"))
+    b must be an integer """
 
 def add_integer(a, b=98):
-    '''
-        adds two integers
-        Arguments:
-        @a: integer or float
-        @b: integer or float
-        both arguments are to be casted into integers
-    '''
+    ''' Returns integer addition a + b
+        Raises type error if a or b is not int or float
+        Floats are casted into integers '''
 
     if type(a) not in [float, int]:
         raise TypeError('a must be an integer')
     if type(b) not in [float, int]:
         raise TypeError('b must be an integer')
-    if (a + 1) == a or (b + 1) == b:
-        raise OverflowError('cannot convert float infinity to integer')
+
     return int(a) + int(b)
