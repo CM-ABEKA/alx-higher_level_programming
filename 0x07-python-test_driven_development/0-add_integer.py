b541ddd8380edfb45d0187feb64345387228d492
cm-abeka <cmabeka2016@gmail.com>
2023-11-11 15:56:43 +0300

    pep 8" "

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -1,13 +1,16 @@
 #!/usr/bin/python3
+
+
 """
 Defines an integer addition function.
 Prototype: def add_integer(a, b=98)
 Returns the sum of the integers a + b
 """
 
+
 def add_integer(a, b=98):
     ''' Raises type error if a or b is not int or float
-        Floats are casted into integers 
+        Floats are casted into integers
     '''
 
     if type(a) not in [float, int]:
