6ffec0acdcb4f7f69b015ad84501345a56dbc4e6
cm-abeka <cmabeka2016@gmail.com>
2023-11-16 22:43:52 +0300

    modified square

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,70 +1,68 @@
-#!/usr/bin/python3
 """Module square for the module that holds the square class"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class sqr, for the class of square, has getters setters,
-    inherits from base>>rectangle"""
+    """Represents a square."""
 
     def __init__(self, size, x=0, y=0, id=None):
-        """Initialization"""
+        """Initializes a new Square object.
+
+        Args:
+            size (int): The size of the square.
+            x (int): The x-coordinate of the square.
+            y (int): The y-coordinate of the square.
+            id (int): The id of the square.
+        """
         super().__init__(size, size, x, y, id)
 
-    def update(self, *args, **kwargs):
-        """Update"""
-        if args is not None and len(args) != 0:
-            self._update_with_args(args)
-        elif kwargs is not None and len(kwargs) != 0:
-            self._update_with_kwargs(kwargs)
-
-    def _update_with_args(self, args):
-        """Update with positional arguments"""
-        n_arg = len(args)
-        if n_arg >= 1:
-            self.id = args[0]
-        if n_arg >= 2:
-            self.size = args[1]
-        if n_arg >= 3:
-            self.x = args[2]
-        if n_arg >= 4:
-            self.y = args[3]
-
-    def _update_with_kwargs(self, kwargs):
-        """Update with keyword arguments"""
-        if "size" in kwargs:
-            self.size = kwargs["size"]
-        if "x" in kwargs:
-            self.x = kwargs["x"]
-        if "y" in kwargs:
-            self.y = kwargs["y"]
-        if "id" in kwargs:
-            self.id = kwargs["id"]
-
     @property
-    def size(self):
-        """Size"""
+    def size(self) -> int:
+        """Gets the size of the square."""
         return self.width
 
     @size.setter
-    def size(self, val):
-        """Size"""
-        self._validate_size(val)
-        self.width = val
-        self.height = val
+    def size(self, value: int):
+        """Sets the size of the square."""
+        self.width = value
+        self.height = value
 
-    def _validate_size(self, val):
-        """Validate size"""
-        if type(val) is not int:
-            raise TypeError('Width must be an integer')
-        elif val <= 0:
-            raise ValueError('Width must be > 0')
+    def update(self, *args, **kwargs):
+        """Updates the square's attributes."""
+        if args:
+            for arg in args:
+                if arg is None:
+                    self.__init__(self.size, self.x, self.y)
+                elif isinstance(arg, int):
+                    if arg == self.id:
+                        self.id = arg
+                    elif arg == self.size:
+                        self.size = arg
+                    elif arg == self.x:
+                        self.x = arg
+                    elif arg == self.y:
+                        self.y = arg
+                    else:
+                        raise ValueError(f"Invalid argument: {arg}")
 
-    def __str__(self):
-        """String representation"""
-        return "[Square] ({}) {}/{} - {}".\
-            format(self.id, self.x, self.y, self.width)
+        if kwargs:
+            for key, value in kwargs.items():
+                if key == "id":
+                    if value is None:
+                        self.__init__(self.size, self.x, self.y)
+                    else:
+                        self.id = value
+                elif key in {"size", "width", "height"}:
+                    self.size = value
+                elif key in {"x", "y"}:
+                    setattr(self, key, value)
+                else:
+                    raise ValueError(f"Invalid keyword argument: {key}")
 
-    def to_dictionary(self):
-        """To dictionary"""
+    def to_dictionary(self) -> dict:
+        """Returns a dictionary representation of the square."""
         return {"id": self.id, "size": self.size, "x": self.x, "y": self.y}
+
+    def __str__(self) -> str:
+        """Returns the string representation of the square."""
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
